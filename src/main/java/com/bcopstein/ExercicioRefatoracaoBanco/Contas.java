dec0364cba8677abcdc61387622093b4f3870901
thallesBueno <42178992+thallesBueno@users.noreply.github.com>
2018-11-20 10:38:09 -0200

    Criei a classe Contas com o padrao singleton

---
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Contas.java
@@ -0,0 +1,25 @@
+package com.bcopstein.ExercicioRefatoracaoBanco;
+
+import java.util.Map;
+
+public class Contas {
+	
+	private Map<Integer,Conta> contas;
+	private Persistencia persistencia;
+	private static Contas instance;
+	
+	private Contas(){
+		persistencia = Persistencia.getInstance();
+		contas = persistencia.loadContas();
+	}
+	
+	public Contas getInstance() {
+		if(instance == null)
+			instance = new Contas();
+		return instance;
+	}
+	
+	public Map<Integer,Conta> getContas() {
+		return contas;
+	}	
+}
