1f1b21e711625c54aa6a6d352cd5312a2fdb9013
GABRIEL PERERIA PAIZ <16201259@pucrs.br>
2018-11-20 11:03:56 -0200

    Mudei as Operaçoes e o app

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Operacoes.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Operacoes.java
@@ -1,5 +1,27 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
 
-public class Operacoes {
+import java.util.List;
 
+public class Operacoes  {
+	
+	private List<Operacao> listOperacoes;
+	private Persistencia persistencia;
+	private static Operacoes instance;
+	
+	private Operacoes() {
+		persistencia = Persistencia.getInstance();
+		listOperacoes = persistencia.loadOperacoes();
+	}
+	
+	public static Operacoes getInstance() {
+		if(instance == null)
+			instance = new Operacoes();
+		return instance;
+	}
+	
+	
+	
+	
+	
+	
 }
